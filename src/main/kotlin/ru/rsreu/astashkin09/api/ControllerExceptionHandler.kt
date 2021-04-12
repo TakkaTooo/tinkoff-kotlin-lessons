a7d498fdc2619b925bbac4392bcae1fbec80264a
Maxim Astashkin <maksim.astash@gmail.com>
2021-04-12 16:53:01 +0300

    some naming fixes

---
+++ src/main/kotlin/ru/rsreu/astashkin09/api/ControllerExceptionHandler.kt
@@ -0,0 +1,18 @@
+package ru.rsreu.astashkin09.api
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.context.request.WebRequest
+import java.lang.NullPointerException
+
+@ControllerAdvice
+class ControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotExistsException::class)
+    fun handleException(ex: EntityNotExistsException, request: WebRequest): ResponseEntity<String> {
+        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    }
+}
